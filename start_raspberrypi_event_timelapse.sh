ded6e77768e5ad1fe5414aa9998630402100b182
Rico Sebastian <r.j.g.sebastian@gmail.com>
2021-10-14 18:23:09 +0200

    removed sudo

--- start_raspberrypi_event_timelapse.sh
+++ start_raspberrypi_event_timelapse.sh
@@ -10,6 +10,6 @@ logfile="${dir}/log_$(date +%Y-%m-%d-%H-%M)"
 echo "logfile of this session: ${logfile}"
 
 # starting script
-sudo /usr/bin/env python3 /opt/raspberrypi_event_timelapse/raspberrypi_event_timelapse.py > "${logfile}"
+/usr/bin/env python3 /opt/raspberrypi_event_timelapse/raspberrypi_event_timelapse.py > "${logfile}"
 
 # see: https://unix.stackexchange.com/a/496370
